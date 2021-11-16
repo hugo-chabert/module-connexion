ecb8710347fd35bb83ef5fb64fcd105cc4e3c889
Hugo <hugo.chabert@laplateforme.io>
2021-11-16 14:37:00 +0100

    Avancement sur HTML de index.php

---
+++ connexion.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="css/inscription.css" rel="stylesheet">
+    <title>Connexion</title>
+</head>
+<body>
+<header>
+    
+</header>
+<main>
+    <a href="index.php">Accueil</a>
+</main>
+<footer>
+
+</footer>    
+</body>
+</html>
